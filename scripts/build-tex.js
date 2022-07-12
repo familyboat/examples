772124f939be15babd57ed00acda322cbce3623e
castle-sky <familyboat@163.com>
2022-07-12 17:07:56 +0800

    1.新增 build-tex.js 脚本 2.新增 cubic-equation-solving quartic-equation-solving 两个案例

---
+++ scripts/build-tex.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const process = require('process');
+const fs = require('fs');
+const { exec } = require("child_process");
+
+const cwd = __dirname;
+const root = path.dirname(cwd);
+let mathematics = path.join(root, 'mathematics');
+
+// 读取参数
+const args = process.argv.slice(2);
+if (args.length === 1) {
+  mathematics = path.join(mathematics, args[0]);
+}
+
+const fileWithSuffix = (file, suffix) => {
+  const stat = fs.lstatSync(file);
+  return stat.isFile() && file.endsWith(suffix)
+}
+
+const travel = (root) => {
+  const files = fs.readdirSync(root);
+
+  // 判断 files 中是否包含 tex 类型的文件
+  const tex = files.filter(file => fileWithSuffix(path.join(root, file), 'tex'));
+  const html = files.filter(file => fileWithSuffix(path.join(root, file), 'html'));
+  if (tex.length === 1 && html.length === 0) {
+    // 执行 pandoc 命令
+    tex.forEach(file => {
+      const absFile = path.join(root, file);
+      const outFile = path.join(root, 'index.html')
+      const command = `pandoc ${absFile} -s --webtex -o ${outFile}`;
+      exec(command);
+      console.log('exec pandoc')
+    })
+  }
+
+  // 递归
+  files.forEach(file => {
+    const absFile = path.join(root, file);
+    const stat = fs.lstatSync(absFile);
+    if (stat.isDirectory()) {
+      travel(absFile);
+    }
+  });
+}
+
+// 从 mathematics 目录开始，递归所有目录，
+// 如果目录中包含 tex 类型的文件，执行 pandoc 命令将其转换为 html 文件。
+travel(mathematics);
